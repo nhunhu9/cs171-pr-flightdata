17dd7229d1ba45e76511aea9a96b02db1054f92c
maza5296 <mkusunoki@college.harvard.edu>
2015-04-14 14:15:22 -0400

    sample line added

--- js/linechart.js
+++ js/linechart.js
@@ -6,6 +6,23 @@ LineChart = function(_parentElement, _data, _metaData, _eventHandler){
   this.eventHandler = _eventHandler;
   this.displayData = [];
 
+  this.sampledata = [{
+    "no_pass": "202",
+    "year": "1990"
+}, {
+    "no_pass": "215",
+    "year": "1995"
+}, {
+    "no_pass": "179",
+    "year": "2000"
+}, {
+    "no_pass": "199",
+    "year": "2005"
+}, {
+    "no_pass": "134",
+    "year": "2010"
+}];
+
   this.margin = {top: 20, right: 0, bottom: 170, left: 60},
   this.width =  650 - this.margin.left - this.margin.right,
   this.height = 440 - this.margin.top - this.margin.bottom;
@@ -22,8 +39,8 @@ LineChart.prototype.initVis = function(){
         .append("g")
         .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
 
-  this.xScale = d3.scale.linear().range([0, this.width]).domain([2000,2010]);
-  this.yScale = d3.scale.linear().range([this.height, 0]).domain([0,100]);
+  this.xScale = d3.scale.linear().range([0, this.width]).domain([1990,2010]);
+  this.yScale = d3.scale.linear().range([this.height, 0]).domain([110,215]);
   this.xAxis = d3.svg.axis()
     .scale(this.xScale)
     .orient("bottom");
@@ -36,8 +53,14 @@ LineChart.prototype.initVis = function(){
         .attr("class", "x axis")
         .attr("transform", "translate(0," + this.height + ")")
 
-    this.svg.append("g")
+  this.svg.append("g")
         .attr("class", "y axis")
+
+  this.line = d3.svg.line()
+    .x(function(d) { return that.xScale(d.year); })
+    .y(function(d) { return that.yScale(d.no_pass); })
+    .interpolate("monotone");;
+
 
   this.updateVis();
   
@@ -73,6 +96,12 @@ LineChart.prototype.updateVis = function(){
 
   this.svg.select(".y.axis")
         .call(this.yAxis)
+
+  this.svg.append("path")
+      .attr("d", that.line(that.sampledata))
+      .attr('stroke', 'green')
+      .attr('stroke-width', 2)
+      .attr('fill', 'none');;
 
 
 }
