6b92d46078a922889e0bae375572338143344083
maza5296 <mkusunoki@college.harvard.edu>
2015-05-02 22:24:33 -0400

    linechart

--- js/linechart.js
+++ js/linechart.js
@@ -1,8 +1,7 @@
-LineChart = function(_parentElement, _data, _metaData, _eventHandler){
+LineChart = function(_parentElement, _data, _eventHandler){
   
   this.parentElement = _parentElement;
   this.data = _data;
-  this.metaData = _metaData;
   this.eventHandler = _eventHandler;
   this.displayData = [];
   this.color = d3.scale.category10()
@@ -11,7 +10,7 @@ LineChart = function(_parentElement, _data, _metaData, _eventHandler){
   this.margin = {top: 20, right: 20, bottom: 170, left: 60},
   this.width =  650 - this.margin.left - this.margin.right,
   this.height = 440 - this.margin.top - this.margin.bottom;
-
+  console.log(_data);
   this.initVis();
 }
 
@@ -24,7 +23,7 @@ LineChart.prototype.initVis = function(){
         .append("g")
         .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
 
-  this.xScale = d3.scale.linear().range([0, this.width]).domain([1990,2010]);
+  this.xScale = d3.scale.linear().range([0, this.width]).domain([2005,2010]);
   this.yScale = d3.scale.linear().range([this.height, 0]);
   
   this.xAxis = d3.svg.axis()
@@ -43,12 +42,6 @@ LineChart.prototype.initVis = function(){
   this.svg.append("g")
         .attr("class", "y axis")
 
-  this.line = d3.svg.line()
-    .x(function(d) { return that.xScale(d["year"]); })
-    .y(function(d) { return that.yScale(d["pass"]); })
-    .interpolate("monotone");;
-
-
   this.wrangleData();
   this.updateVis();
   
@@ -56,7 +49,38 @@ LineChart.prototype.initVis = function(){
 
 
 LineChart.prototype.wrangleData= function(_filterFunction){
-  this.displayData = this.filterAndAggregate("Japan");
+    var tmp = this.filterAndAggregate("Japan"); 
+    console.log(tmp);
+    var that = this;
+
+    this.displayData = tmp.map(function(d){
+        return {
+          name: d["name"],
+          continent: d["continent"],
+          data : [
+              {
+                "year": "2005",
+                "value": d["data"]["Arrivals"]["2005"],
+              },{
+                "year": "2006",
+                "value": d["data"]["Arrivals"]["2006"],
+              },{
+                "year": "2007",
+                "value": d["data"]["Arrivals"]["2007"],
+              },{
+                "year": "2008",
+                "value": d["data"]["Arrivals"]["2008"],
+              },{
+                "year": "2009",
+                "value": d["data"]["Arrivals"]["2009"],
+              },{
+                "year": "2010",
+                "value": d["data"]["Arrivals"]["2010"],
+              },
+          ]
+        }
+    });
+    console.log(that.displayData);
 }
 
 LineChart.prototype.filterAndAggregate = function(_filter){
@@ -66,57 +90,19 @@ LineChart.prototype.filterAndAggregate = function(_filter){
     var that = this;
 
     var filteredData =  this.data.filter(function(d) {
-      return d["departure_country"] == filter;
+      return d["name"] == filter;
     });
-    
-    nested_data = d3.nest()
-      .key(function(d){return d.departure_city})
-      .rollup(function(leaves){ return{
-        "no_1990": d3.sum(leaves, function(d){return d.no_1990}),
-        "no_1995": d3.sum(leaves, function(d){return d.no_1995}),
-        "no_2000": d3.sum(leaves, function(d){return d.no_2000}),
-        "no_2005": d3.sum(leaves, function(d){return d.no_2005}),
-        "no_2010": d3.sum(leaves, function(d){return d.no_2010}),
-      }})
-      .entries(filteredData);
-    nested_data.sort(function(a, b) {
-              return d3.descending(a["values"]["no_1990"], b["values"]["no_1990"]);});
-
-    var displayData = [];
-    var city_data = [];
-    nested_data.map(function(d, i){
-      if(i < 5) {
-        var age = [
-          {
-            "year": "1990",
-            "pass": d["values"]["no_1990"],
-          },{
-            "year": "1995",
-            "pass": d["values"]["no_1995"],
-          },{
-            "year": "2000",
-            "pass": d["values"]["no_2000"],
-          },{
-            "year": "2005",
-            "pass": d["values"]["no_2005"],
-          },{
-            "year": "2010",
-            "pass": d["values"]["no_2010"],
-          },
-          ];
-        city_data = [{"city": d["key"], "data": age}];
-        displayData = displayData.concat(city_data);
-      }
-    })
-    return displayData;
+    console.log(filteredData[0]["data"]["Arrivals"]);
+    return filteredData;
 }
 
 LineChart.prototype.updateVis = function(){
   var that = this;
+  debugger;
 
-  var max = d3.max(that.displayData[0]["data"], function(d){return d.pass})
-  var min = d3.min(that.displayData[4]["data"], function(d){return d.pass})
-  this.yScale.domain([min, max])
+  var max = d3.max(that.displayData[0]["data"], function(d){return d["value"]})
+  var min = d3.min(that.displayData[0]["data"], function(d){return d["value"]})
+  this.yScale.domain([parseInt(min), parseInt(max)])
 
   this.svg.select(".x.axis")
         .call(this.xAxis)
@@ -125,12 +111,18 @@ LineChart.prototype.updateVis = function(){
         .call(this.yAxis)
 
   for (i = 0; i < 5; i++){
+
+    this.line = d3.svg.line()
+    .x(function(d) { return that.xScale(parseInt(d["year"])); })
+    .y(function(d) { debugger; return that.yScale(parseInt(d["value"])); })
+    .interpolate("monotone");;
+    console.log(that.displayData[0]["data"])
     this.svg.append("path")
-    .attr('d', that.line(that.displayData[i]["data"]))
+    .attr('d', that.line(that.displayData[0]["data"]))
     .attr('stroke', that.color(i))
     .attr('stroke-width', 2)
     .attr('fill', 'none')
-    .text(that.displayData[i]["city"]);;
+    .text(that.displayData[0]["name"]);;
   }
 
 }
--- js/toCapital.js
+++ js/toCapital.js
@@ -1,3 +1,24 @@
 String.prototype.capitalizeFirstLetter = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 }
+
+String.prototype.toTitleCase = function() {
+    return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
+// Object.prototype.toArray = function() {
+// 	var _Array = new Array();
+//     for(var name in this) {
+//            _Array[name] = this[name];
+//    	}
+//    	return _Array;
+// };
+
+
+function toArray(_Object){
+       var _Array = new Array();
+       for(var name in _Object){
+               _Array[name] = _Object[name];
+       }
+       return _Array;
+}
