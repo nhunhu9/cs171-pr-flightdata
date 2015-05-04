22a74eebbfbd19f0a255eee7f8f36c3722732b4b
Zeno Ziemke <ziemke@college.harvard.edu>
2015-05-03 23:17:03 -0400

    bind to eventhandler

--- js/worldmap.js
+++ js/worldmap.js
@@ -1,4 +1,4 @@
-WorldMap = function(_parentElement, _data, _metaData, _countriesToCountries, _eventHandler){
+WorldMap = function(_parentElement, _data, _metaData, _countriesToCountries, _completeTable, _eventHandler){
 
   var that = this;
   
@@ -6,6 +6,7 @@ WorldMap = function(_parentElement, _data, _metaData, _countriesToCountries, _ev
   this.data = _data;
   this.metaData = _metaData;
   this.countriesToCountries = _countriesToCountries;
+  this.completeTable = _completeTable;
   this.eventHandler = _eventHandler;
   this.displayData = [];
 
@@ -141,11 +142,18 @@ WorldMap.prototype.initVis = function(){
  this.wrangleData(null);
 
   this.updateVis();
+
+  this.showAirlineRoutes("All Nippon Airways");
 }
 
 // level: default: city, alternative: country
 WorldMap.prototype.wrangleData= function(_filterFunction, level){
   this.displayData = this.filterAndAggregate(_filterFunction, level);
+}
+
+WorldMap.prototype.showAirlineRoutes = function(airline_name ) {
+  this.displayData = this.completeTable.filter(function(d) { return d.airline_name == airline_name; })
+  this.updateVis();
 }
 
 WorldMap.prototype.filterAndAggregate = function(_filter, level){
