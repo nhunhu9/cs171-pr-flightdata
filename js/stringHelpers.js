47868c39aee8fbbaad27e97a6683e09f3035db69
maza5296 <mkusunoki@college.harvard.edu>
2015-05-05 23:47:40 -0400

    FINISHED

--- js/stringHelpers.js
+++ js/stringHelpers.js
@@ -6,22 +6,6 @@ String.prototype.toTitleCase = function() {
     return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
 
-// Object.prototype.toArray = function() {
-// 	var _Array = new Array();
-//     for(var name in this) {
-//            _Array[name] = this[name];
-//    	}
-//    	return _Array;
-// };
-
 String.prototype.removeComma = function() {
     return(this.replace(/,/g,''));
-}
-
-function toArray(_Object){
-       var _Array = new Array();
-       for(var name in _Object){
-               _Array[name] = _Object[name];
-       }
-       return _Array;
 }
